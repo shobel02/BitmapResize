1b80561d7464ad4995feb0bdce410d6c81d98e49
shobel02 <soukyuf02@hotmail.co.jp>
2019-09-10 01:02:53 +0900

    初回登録

---
+++ BitmapResize/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace BitmapResize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "変換";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ファイルをドロップ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(85, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(768, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox1_DragEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(686, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 309);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 47);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 39);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "画像サイズ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "縦";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "横";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 123);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(192, 39);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 33);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "画像取り込み";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(189, 19);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "実際のサイズで画像を表示";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(306, 115);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(374, 174);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Location = new System.Drawing.Point(85, 127);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 47);
+            this.panel2.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 50);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "画像保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "画像保存形式";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox5);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.textBox4);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.comboBox1);
+            this.panel3.Location = new System.Drawing.Point(85, 357);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(532, 154);
+            this.panel3.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(182, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "保存先";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(176, 51);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.Text = "temp";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(309, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 15);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "保存ファイル名";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(312, 51);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 21;
+            this.textBox5.Text = "デフォルト";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(213, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 15);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "画像変換オプション";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(212, 47);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(159, 23);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(261, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 33);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "取得画像取り消し";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1177, 625);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "取得画像取り消し";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ BitmapResize/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BitmapResize
+{
+    public partial class Form1 : Form
+    {        
+        /// <summary>
+        /// 画像保存リスト
+        /// </summary>
+        List<System.Drawing.Imaging.ImageFormat> imageFormats = new List<System.Drawing.Imaging.ImageFormat>();
+        List<System.Drawing.Drawing2D.InterpolationMode> interpolationModes = new List<System.Drawing.Drawing2D.InterpolationMode>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /// 画像の保存形式セット
+            imageFormats.Add(System.Drawing.Imaging.ImageFormat.Jpeg);
+            imageFormats.Add(System.Drawing.Imaging.ImageFormat.Png);
+            imageFormats.Add(System.Drawing.Imaging.ImageFormat.Bmp);
+            imageFormats.Add(System.Drawing.Imaging.ImageFormat.Tiff);
+            imageFormats.Add(System.Drawing.Imaging.ImageFormat.Icon);
+
+            int cnt = 0;
+            /// コンボボックスに画像保存形式をセット
+            foreach (var item in imageFormats)
+            {
+                comboBox1.Items.Add(imageFormats[cnt]);
+                cnt++;
+            }
+
+            /// 初期値セット
+            comboBox1.SelectedIndex = 0;
+
+            /// 画像の変換形式セット
+            interpolationModes.Add(System.Drawing.Drawing2D.InterpolationMode.Default);
+            interpolationModes.Add(System.Drawing.Drawing2D.InterpolationMode.High);
+            interpolationModes.Add(System.Drawing.Drawing2D.InterpolationMode.Low);
+            interpolationModes.Add(System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic);
+            interpolationModes.Add(System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear);
+            interpolationModes.Add(System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor);
+
+            cnt = 0;
+            /// コンボボックスに画像変換形式をセット
+            foreach (var item in imageFormats)
+            {
+                comboBox2.Items.Add(interpolationModes[cnt]);
+                cnt++;
+            }
+
+            /// 初期値セット
+            comboBox2.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// 取得画像表示イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /// 画像取得
+                Bitmap bmp = new Bitmap(textBox1.Text);
+                /// 画像から横幅取得
+                textBox2.Text = bmp.Width.ToString();
+                /// 画像から縦幅取得
+                textBox3.Text = bmp.Height.ToString();
+                /// 画像表示
+                pictureBox1.Image = bmp;                
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        /// <summary>
+        /// 画像変換
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /// 描画先とするImageオブジェクトを作成する
+                Bitmap canvas = new Bitmap(int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+
+                /// ImageオブジェクトのGraphicsオブジェクトを作成する
+                Graphics g = Graphics.FromImage(canvas);
+
+                /// Bitmapオブジェクトの作成
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                /// 補間方法を指定する
+                g.InterpolationMode = (System.Drawing.Drawing2D.InterpolationMode)comboBox2.SelectedItem;
+                /// 画像を変換して描画する
+                g.DrawImage(image, 0, 0, int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+
+                /// BitmapとGraphicsオブジェクトを破棄
+                image.Dispose();
+                g.Dispose();
+
+                /// PictureBox1に表示する
+                pictureBox1.Image = canvas;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("画像取り込みエラーまたはその他のエラー");
+            }
+        }
+
+        /// <summary>
+        /// チェックボックス状態変化イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            /// チェックボックスがONの時
+            if (checkBox1.Checked == true)
+            {
+                /// 原寸大で表示
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+            else
+            {
+                /// ピクチャーボックスのサイズに合わせて表示
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        /// <summary>
+        /// 画像保存イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>C
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /// 表示している画像からビットマップ生成
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+                /// 出力パス取得
+                string path = "C:\\" + textBox4.Text;
+
+                /// 出力先のディレクトリがなかった場合は作成
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                /// 画像保存
+                bmp.Save("C:\\" + textBox4.Text + "\\" + textBox5.Text + "." + comboBox1.SelectedItem.ToString(), (System.Drawing.Imaging.ImageFormat)comboBox1.SelectedItem);
+                bmp.Dispose();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("未入力の項目またはその他のエラー");
+            }
+        }
+
+        /// <summary>
+        /// 画像取り消し処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /// 初期化
+                pictureBox1.Image = null;
+                textBox1.Text = "";
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        #region ドラック＆ドロップ処理
+        //ListBox1のDragEnterイベントハンドラ
+        private void ListBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            //コントロール内にドラッグされたとき実行される
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                //ドラッグされたデータ形式を調べ、ファイルのときはコピーとする
+                e.Effect = DragDropEffects.Copy;
+            else
+                //ファイル以外は受け付けない
+                e.Effect = DragDropEffects.None;
+        }
+
+        //ListBox1のDragDropイベントハンドラ
+        private void ListBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            //コントロール内にドロップされたとき実行される
+            //ドロップされたすべてのファイル名を取得する
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //textBoxに追加する
+            textBox1.Text = fileName[0];
+        }
+        #endregion
+    }
+}
